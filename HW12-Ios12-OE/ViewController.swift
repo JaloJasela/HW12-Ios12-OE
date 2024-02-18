2aa89e08a61c78e1ae0dc7f4615e83ab2b068476
JaloJasela <jasela@yandex.ru>
2024-02-18 19:39:10 +0300

    Initial Commit

---
+++ HW12-Ios12-OE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12-Ios12-OE
+//
+//  Created by IMac on 18.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
